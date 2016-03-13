48dad53aeea221a3ef5d85ff593112fb075325bf
Kyl Wellman <kyl@gratafy.com>
2016-03-12 18:29:31 -0800

    add validator middleware, test, and sample

---
+++ src/sample/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"github.com/dgrijalva/jwt-go"
+	"github.com/gin-gonic/gin"
+	"jwtauth"
+	"time"
+)
+
+const superSecretKey = "CAFEBEEF"
+
+type claimsObject struct {
+	auth bool
+}
+
+func main() {
+	r := gin.New()
+
+	r.POST("/tokens", makeToken)
+	r.GET("/private", jwtauth.Validator([]byte(superSecretKey), jwt.SigningMethodHS256), privateHandler)
+
+	r.Run()
+}
+
+func privateHandler(c *gin.Context) {
+	claims := c.MustGet("claims").(map[string]interface{})
+	auth, ok := claims["auth"].(bool)
+	if !ok || !auth {
+		c.AbortWithError(401, fmt.Errorf("Request Unauthorized"))
+	}
+
+	c.JSON(200, gin.H{
+		"request status": "authorized",
+	})
+}
+
+func makeToken(c *gin.Context) {
+	token := jwt.New(jwt.SigningMethodHS256)
+	token.Claims["auth"] = true
+	token.Claims["exp"] = time.Now().Add(time.Hour).Unix()
+
+	tokenString, err := token.SignedString([]byte(superSecretKey))
+
+	if err != nil {
+		fmt.Printf("Error: %s\n", err)
+	}
+
+	fmt.Printf("Token: %#v\n", token)
+	fmt.Printf("key: %s\n", superSecretKey)
+	fmt.Printf("Token string: %s\n", tokenString)
+
+	c.JSON(200, gin.H{
+		"token": tokenString,
+	})
+}
